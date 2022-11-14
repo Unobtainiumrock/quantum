baa58bc9f11ed418311859f65c4a74ac2397f0c1
unobtainiumrock <nicholas.fleischahau@my.smccd.edu>
2022-11-14 11:50:33 -0800

    add macbook code

---
+++ tmp/api-test.js
@@ -0,0 +1,12 @@
+(async () => {
+  const fetch = require('fetch');
+  const parser = require('xml2json');
+
+  // fetch.fetchUrl('http://export.arxiv.org/api/query?search_query=all:electron', (error, meta, body) => {
+  //   // console.log(typeof body);
+  //   // console.log(typeof body.toString);
+  //   console.log(body.toString());
+  // });
+
+
+})();
